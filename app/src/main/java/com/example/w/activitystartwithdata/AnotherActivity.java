303c17838674eece47d9c69ca97147c9cbe1bc15
w <1379853502@qq.com>
2016-09-06 08:50:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/w/activitystartwithdata/AnotherActivity.java
@@ -0,0 +1,34 @@
+package com.example.w.activitystartwithdata;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Button button=(Button)findViewById(R.id.buttonReturn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(AnotherActivity.this,MainActivity.class);
+                startActivity(intent1);
+                finish();
+
+            }
+        });
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age",19);
+        Toast.makeText(this,name+age,Toast.LENGTH_LONG).show();
+    }
+}
